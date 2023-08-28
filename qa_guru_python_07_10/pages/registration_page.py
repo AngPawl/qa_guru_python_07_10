b8cf47a8750f61b47ac9f02f96a29dc8a5454029
Anhelina Babareka <anhielina.babareka@gmail.com>
2023-08-28 20:46:11 +0300

    refactor to Fluent PageObject style

--- qa_guru_python_07_10/pages/registration_page.py
+++ qa_guru_python_07_10/pages/registration_page.py
@@ -19,20 +19,25 @@ class RegistrationPage:
 
     def fill_first_name(self, first_name):
         browser.element('#firstName').type(first_name)
+        return self
 
     def fill_last_name(self, last_name):
         browser.element('#lastName').type(last_name)
+        return self
 
     def fill_user_email(self, email):
         browser.element('#userEmail').type(email)
+        return self
 
     def fill_gender(self, gender):
         browser.all('[name=gender]').element_by(have.value(gender)).element(
             '..'
         ).click()
+        return self
 
     def fill_phone_number(self, phone_number):
         browser.element('#userNumber').type(phone_number)
+        return self
 
     def fill_date_of_birth(self, month, year, day):
         browser.element('#dateOfBirthInput').click()
@@ -47,30 +52,37 @@ class RegistrationPage:
         browser.element(
             f'.react-datepicker__day--00{day}:not(.react-datepicker__day--outside-month)'
         ).click()
+        return self
 
     def fill_subjects(self, subject):
         browser.element('#subjectsInput').type(subject).press_tab()
+        return self
 
     def fill_hobbies(self, hobby):
         browser.all('[id^=hobbies][type=checkbox]+label').element_by(
             have.exact_text(hobby)
         ).click()
+        return self
 
     def upload_picture(self, picture_path):
         browser.element('#uploadPicture').set_value(resource_path(picture_path))
+        return self
 
     def fill_current_address(self, address):
         browser.element('#currentAddress').type(address).press_tab()
+        return self
 
     def fill_state(self, state):
         browser.element('#state').click()
 
         self.drop_down_options.element_by(have.exact_text(state)).click()
+        return self
 
     def fill_city(self, city):
         browser.element('#city').click()
 
         self.drop_down_options.element_by(have.exact_text(city)).click()
+        return self
 
     def submit_form(self):
         browser.element('#submit').perform(command.js.click)
